2f2df2b67bf0ae2e916bf2a3930e1d66872476e2
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-04-01 18:32:04 +0100

    add linting

--- src/components/ChatGPT.tsx
+++ src/components/ChatGPT.tsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
+// I want a named export react typescript component called ChatGPT that draws the bitcoin price for the last 5 years. The typescript must not have any implicit anys.
+
+import axios, { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 interface PriceData {
   time: number;
@@ -19,12 +13,12 @@ export const ChatGPT: React.FC = () => {
   const [priceData, setPriceData] = useState<PriceData[]>([]);
 
   useEffect(() => {
-    const fetchPriceData = async () => {
-      const response = await axios.get(
-        "https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-04-01&end=2021-04-01"
+    const fetchPriceData = async (): Promise<void> => {
+      const response: AxiosResponse<{ bpi: Record<string, number> }> = await axios.get(
+        'https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-04-01&end=2021-04-01'
       );
-      const data = response.data.bpi;
-      const formattedData = Object.keys(data).map((date) => ({
+      const data: Record<string, number> = response.data.bpi;
+      const formattedData: PriceData[] = Object.keys(data).map((date: string) => ({
         time: new Date(date).getTime(),
         price: data[date],
       }));
@@ -34,19 +28,18 @@ export const ChatGPT: React.FC = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ width: '100%', maxWidth: '100vw', overflowX: 'hidden' }}>
       <h1>Bitcoin Price (last 5 years)</h1>
-      <LineChart width={800} height={400} data={priceData}>
-        <XAxis
-          dataKey="time"
-          tickFormatter={(time) => new Date(time).toLocaleDateString()}
-        />
-        <YAxis dataKey="price" />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="price" stroke="#8884d8" />
-      </LineChart>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={priceData}>
+          <XAxis dataKey="time" tickFormatter={(time: number) => new Date(time).toLocaleDateString()} />
+          <YAxis dataKey="price" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="price" stroke="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
