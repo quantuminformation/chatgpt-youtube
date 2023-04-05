f5349ca1d67f1a84d9e4273ec464bfbc3d11cb69
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-04-05 21:26:56 +0300

    first useReducer

--- src/components/ChatGPT.tsx
+++ src/components/ChatGPT.tsx
@@ -1,31 +1,78 @@
-// I want a named export react typescript component called ChatGPT that draws the bitcoin price for the last 5 years. The typescript must not have any implicit anys.
+// I want a named export react typescript component called ChatGPT that draws the bitcoin price for the last 5 years. The typescript must not have any implicit anys. Use only useReducer.
 
+import React, { useEffect, useReducer } from 'react';
 import axios, { AxiosResponse } from 'axios';
-import React, { useEffect, useState } from 'react';
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 interface PriceData {
   time: number;
   price: number;
 }
 
+type Action = { type: 'FETCH_SUCCESS'; payload: PriceData[] } | { type: 'FETCH_FAILURE'; error: Error };
+
+type State = {
+  loading: boolean;
+  priceData: PriceData[];
+  error: Error | null;
+};
+
+const initialState: State = {
+  loading: true,
+  priceData: [],
+  error: null,
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS':
+      return {
+        loading: false,
+        priceData: action.payload,
+        error: null,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        loading: false,
+        priceData: [],
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
 export const ChatGPT: React.FC = () => {
-  const [priceData, setPriceData] = useState<PriceData[]>([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     const fetchPriceData = async (): Promise<void> => {
-      const response: AxiosResponse<{ bpi: Record<string, number> }> = await axios.get(
-        'https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-04-01&end=2021-04-01'
-      );
-      const data: Record<string, number> = response.data.bpi;
-      const formattedData: PriceData[] = Object.keys(data).map((date: string) => ({
-        time: new Date(date).getTime(),
-        price: data[date],
-      }));
-      setPriceData(formattedData);
+      try {
+        const response: AxiosResponse<{ bpi: Record<string, number> }> = await axios.get(
+          'https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-04-01&end=2021-04-01'
+        );
+        const data: Record<string, number> = response.data.bpi;
+        const formattedData: PriceData[] = Object.keys(data).map((date: string) => ({
+          time: new Date(date).getTime(),
+          price: data[date],
+        }));
+        dispatch({ type: 'FETCH_SUCCESS', payload: formattedData });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAILURE', error });
+      }
     };
     fetchPriceData();
   }, []);
+
+  const { loading, priceData, error } = state;
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <div style={{ width: '100%', maxWidth: '100vw', overflowX: 'hidden' }}>
