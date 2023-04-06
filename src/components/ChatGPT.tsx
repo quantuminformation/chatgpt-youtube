36f39ed8360be9ea2fe79d18865db3ea0b1c1c3f
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-04-06 10:22:03 +0300

    first vitest try

--- src/components/ChatGPT.tsx
+++ src/components/ChatGPT.tsx
@@ -1,4 +1,4 @@
-// I want a named export react typescript component called ChatGPT that draws the bitcoin price for the last 5 years. The typescript must not have any implicit anys. Use only useReducer. Catch params must have the correct error type.
+// I want a named export react typescript component called ChatGPT that draws the bitcoin price for the last 5 years. The typescript must not have any implicit anys. Use only useReducer.
 
 import React, { useEffect, useReducer } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    transformMode: {
+      web: [/.[jt]sx?/],
+    },
+    deps: {
+      registerNodeLoader: true,
+    },
+    threads: false,
+    isolate: false,
+  },
+});
