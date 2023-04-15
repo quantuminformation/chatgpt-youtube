97abd90192d2b9617ee8b553d79358d43ba202ff
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-04-15 23:25:13 +0200

    part 7, intro to solid

--- AI-templates/components/App.md
+++ AI-templates/components/App.md
@@ -1,3 +1,5 @@
-Write me named export react TSX full width component called App.tsx which displays a hello world in the center. 
+Write me named export solid.js TSX full width component called App.tsx which displays a hello world in the center of the screen. Use kebab case style property names where needed encased in strings.
+
+Don't style any unnecessary elements.
 
 Styles should be in the same component
--- solid/src/App.tsx
+++ solid/src/App.tsx
@@ -1,27 +1,18 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Component, createSignal } from 'solid-js';
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+    const [styles, setStyles] = createSignal({
+        display: 'flex',
+        'justify-content': 'center',
+        'align-items': 'center',
+        height: '100vh',
+    });
+
+    return (
+        <div style={styles()}>
+            <h1 >Hello World!</h1>
+        </div>
+    );
 };
 
 export default App;
